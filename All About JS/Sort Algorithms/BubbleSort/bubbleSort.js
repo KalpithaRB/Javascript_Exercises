3b95f727228e9e814be36468af3918aa8c7feff5
Kalpitha <kalpitharb04@gmail.com>
2023-03-26 19:39:59 +0530

    Added bubble sort

--- All About JS/Sort Algorithms/BubbleSort/bubbleSort.js
+++ All About JS/Sort Algorithms/BubbleSort/bubbleSort.js
@@ -1,22 +1,24 @@
-function swap(arr, first_Index, second_Index){
-    var temp = arr[first_Index];
-    arr[first_Index] = arr[second_Index];
-    arr[second_Index] = temp;
-}
-
 function bubble_Sort(arr){
 
-    var len = arr.length,
-        i, j, stop;
-
-    for (i=0; i < len; i++){
-        for (j=0, stop=len-i; j < stop; j++){
-            if (arr[j] > arr[j+1]){
-                swap(arr, j, j+1);
+    for(var i = 0; i < arr.length; i++){
+        
+        // Last i elements are already in place
+        for(var j = 0; j < ( arr.length - i -1 ); j++){
+            
+            // Checking if the item at present iteration
+            // is greater than the next iteration
+            if(arr[j] > arr[j+1]){
+                
+            // If the condition is true then swap them
+            var temp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j+1] = temp
             }
         }
-    }
-
+        }
     return arr;
 }
+
+console.log([3, 0, 2, 5, -1, 4, 1]);
 console.log(bubble_Sort([3, 0, 2, 5, -1, 4, 1]));
+

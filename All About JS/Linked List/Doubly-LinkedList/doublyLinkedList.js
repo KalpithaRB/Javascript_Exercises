9aec6664c28bfe645992983bb8f2d952525f915c
Kalpitha <kalpitharb04@gmail.com>
2023-03-27 11:28:40 +0530

    added searching the node in singly linked list

--- All About JS/Linked List/Doubly-LinkedList/doublyLinkedList.js
+++ All About JS/Linked List/Doubly-LinkedList/doublyLinkedList.js
@@ -25,6 +25,38 @@ class DoublyLinkedList {
 		this.length++;
 	}
 
+	//Search function
+	search( head_ref, x)
+	{
+ 
+    // Stores head Node
+    var temp = head_ref;
+ 
+    // Stores position of the integer
+    // in the doubly linked list
+    var pos = 0;
+ 
+    // Traverse the doubly linked list
+    while (temp.data != x
+           && temp.next != null) {
+ 
+        // Update pos
+        pos++;
+ 
+        // Update temp
+        temp = temp.next;
+    }
+ 
+    // If the integer not present
+    // in the doubly linked list
+    if (temp.data != x)
+        return -1;
+ 
+    // If the integer present in
+    // the doubly linked list
+    return (pos + 1);
+	};
+
     printList() {
 		let current = this.head;
 		let result = [];
@@ -34,11 +66,11 @@ class DoublyLinkedList {
 		}
 		console.log(result.join(' '));
 		return this;
-	}
+	};
 
 	size() {
 		return this.length;
-	}
+	};
 
   is_Empty(){
       return this.length === 0;
@@ -64,3 +96,5 @@ numList.printList();
 // console.log(is_Empty(numList));
 console.log("Check if the list empty or not");
 console.log(numList.is_Empty());
+console.log(("The position of 2 in the array is "+numList.search(7,7)));
+
--- All About JS/Linked List/Singly-LinkedList/singlyLinkedList.js
+++ All About JS/Linked List/Singly-LinkedList/singlyLinkedList.js
@@ -27,6 +27,49 @@ class SinglyLinkedList {
         return (this.Head==null)
     } 
 
+    displayList(){
+        let node = this.Head;
+        var str = ""
+        while (node) {
+            str += node.data + " -> ";
+            node = node.next;
+        }
+        str += "NULL"
+        console.log(str);  
+      }
+    
+      insertAt(index, newNode){
+        let node = this.Head;
+        if(index==0) {
+            newNode.next = node;
+            this.head = newNode;
+            return;
+        }
+        while(--index){
+            if(node.next!==null)
+                node = node.next;
+            else
+                throw Error("Index Out of Bound");
+        }
+        let tempVal = node.next;
+        node.next = newNode;
+        newNode.next = tempVal;
+        }
+    
+        get_node(index){
+            let node = this.Head;
+            if(index==0) {
+                return this.Head;
+            }
+            while(index--){
+                if(node.next!==null)
+                    node = node.next;
+                else
+                    throw Error("Index Out of Bound");
+            }
+            return node;
+        } 
+
 }
 
 let numList = new SinglyLinkedList();
@@ -40,6 +83,23 @@ numList.add(new Node(7));
 
 console.log("Check if the list empty or not");
 console.log(numList.is_Empty());
+console.log("The Lists items");
+numList.displayList();
+
+console.log("Insert 4 at index position 1:")
+numList.insertAt(1, new Node(4));
+console.log("List items after inserting number 4 at the index 1");
+numList.displayList();
+// console.log("Insert 5 at index position 3:")
+// numList.insertAt(3, new Node(5));
+// numList.displayList();
+// console.log("Insert 6 at index position 6:")
+// numList.insertAt(6, new Node(6));
+// numList.displayList();
+
+console.log("Node details at index 4:")
+console.log(" ");
+console.log(numList.get_node(4));
 
 
 
